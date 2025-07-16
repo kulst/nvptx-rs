b178f540e2213bc846ccca5bc78f6a77c00d9d10
kulst <kulst@mailbox.org>
2025-07-16 22:09:22 +0200

    Small optimizations and documentation added to reduction kernel

--- kernels/src/reduction.rs
+++ kernels/src/reduction.rs
@@ -1,17 +1,6 @@
-use core::arch::nvptx::vprintf;
-use core::cell::UnsafeCell;
-use core::ffi::c_void;
-use core::mem::transmute;
 use mdarray::expr::for_each;
-use mdarray::expr::Apply;
 use mdarray::Dense;
 use mdarray::DenseMapping;
-use mdarray::Dyn;
-use mdarray::StepRange;
-use mdarray::Strided;
-use mdarray::StridedMapping;
-use mdarray::View;
-use mdarray::ViewMut;
 use num::{traits::float::FloatCore, FromPrimitive};
 
 use simtarray::nvptx::Block;
@@ -31,56 +20,63 @@ where
 {
     // initialize dynamic shared memory
     let mut dyn_smem = DynSmem::new();
+    // Create one one-dimensional SimtArray per Grid (it is the same in the
+    // whole Grid) which is already initialized
     let input = SimtArray::<_, Grid, Init, Dense, _>::new_unchecked(
         input as *mut T,
         DenseMapping::new((n,)),
     );
+    // Create a disjunct view into the input for each Thread determined by
+    // the Xyz projection for the only dimension 0 (might be None for some threads
+    // if the number of threads in the grid is larger than n)
     let input_view = input.view::<Thread, (Xyz,)>();
-    //
-    let n_local = <Xyz as Projection<Thread, Block>>::dim().as_();
-    let local_sum = DynSmem::get_chunk::<T>(&mut dyn_smem, n_local);
-    let local_sum = SimtArray::<_, Block, Uninit, Dense, _>::new_unchecked(
-        local_sum,
-        DenseMapping::new((n_local,)),
-    );
-    // Initialize local_sum with first input value or zero if gtid >= n
-    let mut local_sum = local_sum.init_with::<Thread, (Xyz,), _>(|_| {
-        if let Some(input_view) = input_view {
-            input_view[0]
-        } else {
-            T::zero()
-        }
-    });
-    let input_view = input_view.map(|input_view| {
-        input_view.into_view(StepRange {
-            range: 1..,
-            step: 1,
-        })
-    });
-    {
-        let mut local_sum_view = local_sum.view_mut::<Thread, (Xyz,)>();
-        if let Some((local_val, input)) =
-            local_sum_view.expr_mut().as_mut().zip(input_view.as_ref())
-        {
-            for_each(input, |input_val| local_val[0] += *input_val)
-        }
+    // Create a thread-local value which reduces all values of the disjunct input
+    // view of the thread
+    let mut local_val = T::zero();
+    if let Some(input_view) = input_view {
+        for_each(input_view, |input_val| local_val += *input_val)
     }
-    // // reduce the local_sum
+    // n_local is the count of threads per block considering all three dimension
+    // It is equivalent to _block_dim_x() * _block_dim_y() * _block_dim_z()
+    let n_local = <Xyz as Projection<Thread, Block>>::dim().as_();
+    // Create one one-dimensional uninitialized SimtArray in shared memory
+    // per Block (it is different for each block) and initialize it with the
+    // thread-local reduced value.
+    // We do not have to explicitely synchronize as this is implicetly done
+    // after the closure is called
+    let mut local_sum = SimtArray::<_, Block, Uninit, Dense, _>::new_unchecked(
+        DynSmem::get_chunk::<T>(&mut dyn_smem, n_local),
+        DenseMapping::new((n_local,)),
+    )
+    .init_with::<Thread, (Xyz,), _>(|_| local_val);
+    // calculating the number of threads which are necessary for the reduction
+    // this is the next smallest power of 2
     let mut nworkers = get_init_worker_count(n_local);
+    // As long as we still need workers..
     while nworkers > 0 {
-        let mut local_sum_view =
+        // For all workers we create a mutable view into the local_sum, the others
+        // get None
+        let mut local_sum_ref_mut =
             local_sum.view_mut_with_limited_quantity::<Thread, (Xyz,)>(nworkers as u32);
-        if let Some(mut local_sum_view) = local_sum_view.expr_mut() {
+        // Only if we are a worker (the view is Some(..)) ..
+        if let Some(mut local_sum_view) = local_sum_ref_mut.expr_mut() {
+            // and we have at least 2 elements in the view
             if local_sum_view.dim(0) >= 2 {
+                // we reduce these two elements into one
                 let other = local_sum_view[1];
                 local_sum_view[0] += other;
             }
         }
+        // We do not have to explicitely synchronize here, as this is done
+        // implicetly when dropping the local_sum_ref_mut after each loop iteration
+
+        // We halve the number of workers
         nworkers /= 2;
     }
-    //
+    // We create an immutable view into the local sum ..
     let local_sum_view = local_sum.expr();
-    // Write block result back into global memory
+    // and only if we are thread 0 inside the block we atomically add our
+    // local_sum to the global sum
     if <Xyz as Projection<Thread, Block>>::idx() == 0 {
         _atomic_add::<T>(output, local_sum_view[0]);
     }
